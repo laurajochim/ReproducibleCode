c3a77b77fd433a5cb3844d8ea43450416ac92ee0
Laura Jochim <l.jochim@umcutrecht.nl>
2026-06-12 11:30:27 +0200

    Added stop to bmi function

--- R/Rfunctions.R
+++ R/Rfunctions.R
@@ -3,18 +3,32 @@
 ######################
 
 # BMI calculations
+#"@description
+#' Returns the BMI
+#' @param weight in kilo
+#' @param height in meters
+#' @return BMi value
+#' @export bmi
+#' @examples
+#' bmi(70, 1.70)"
  bmi <- function(weight, height){
   message("Returns the BMI")
 
   if(any(height >3)){
   warning("Height must be in meters, eg. 1.7 instead of 170")
 }
+
+if (!is.numeric(weight) | !is.numeric(height)) {
+  stop("Both weight and height must be numeric.")
+}
+
 weight/(height)^2
 
 }
 
 
 #Convert categorical vairbales in to binary numeric variables
+#'
 binary <- function(variable, reference) {
   message("Returns a binary vairable with 1 as reference.")
 
